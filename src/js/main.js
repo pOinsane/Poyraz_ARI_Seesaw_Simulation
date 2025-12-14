217ae8dcda9c543800403fa46d1f5d2f8801593d
pOinsane <poyraz.ari@ug.bilkent.edu.tr>
2025-12-14 23:17:17 +0300

    Added event listeners for limiting clickable area by using the position of mouse and for click event without the functionality of creating weights

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,42 @@
-/*const page = document.getElementById("page");
+const simContainer = document.getElementById("sim_container");
+const seesaw = document.getElementById("seesaw");
 
-const spheres = [];
+//Simulation Container toplamda 700 pixel, seesaw ise 400 pixel
+//Simulation Containerın merkezi, seesaw'un da merkezi olduğu için tam olarak 350 pixel sağa gidildiğinde seesaw'un merkezine gelmiş oluruz
+//Seesaw da ağırlık merkezinin sağına ve soluna 200er pixel olarak bölündüğü için
+//Container'ın 150 pixel sağına gittiğimizde, Seesaw'un başlangıcına ulaşırız. 
+//Container sabit size'a sahip olduğu için ekran çözünürlüğü arttığında da sorunsuz bi şekilde çalışacaktır
+//Ekrandaki herhangi bir elementin yeri kaymayacaktır. 
 
-page.addEventListener("click", (event) => {
-    const weight = Math.floor(Math.random() * 9) + 1;
+    const containerPos = simContainer.getBoundingClientRect();
+    const center = simContainer.offsetWidth/2;
+    const seesawHalfWidth = 200;
 
-    const sphereObject = document.createElement("div");
-    sphereObject.className = "sphere";
-    sphereObject.textContent = weight + "kg";
+    const clickableAreaLeftBound = center - seesawHalfWidth;  
+    const clickableAreaRightBound = center + seesawHalfWidth;
+
+simContainer.addEventListener('mousemove', function(event){
+
+    const mouseX = event.clientX - containerPos.left;
+
+    if(mouseX >= clickableAreaLeftBound && mouseX <= clickableAreaRightBound)
+    {
+        simContainer.style.cursor = 'pointer';
+    } else {
+        simContainer.style.cursor = 'default';
+    }
+
 });
-*/
+
+simContainer.addEventListener('click', function(event)
+{
+
+    const clickX = event.clientX - containerPos.left;
+
+    if(clickX >= clickableAreaLeftBound && clickX <= clickableAreaRightBound)
+    {
+        //weight olusturulcak
+    }
+
+});
+
