98be39734eff66317f8897fb47803aa5b8ce50eb
pOinsane <poyraz.ari@ug.bilkent.edu.tr>
2025-12-14 21:37:45 +0300

    Foundation of UI Layout

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/*const page = document.getElementById("page");
+
+const spheres = [];
+
+page.addEventListener("click", (event) => {
+    const weight = Math.floor(Math.random() * 9) + 1;
+
+    const sphereObject = document.createElement("div");
+    sphereObject.className = "sphere";
+    sphereObject.textContent = weight + "kg";
+});
+*/
